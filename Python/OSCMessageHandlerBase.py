912d846c4342c20b4d7b899d49780b424fc03255
Vou Theophanous <vou@Vous-MacBook-Pro.local>
2025-04-23 14:08:00 -0700

    specifying class var type

--- Python/OSCMessageHandlerBase.py
+++ Python/OSCMessageHandlerBase.py
@@ -1,6 +1,9 @@
+from typing import List
+from datafeel.device import Dot
+
 class OSCMessageHandlerBase:
 
-    def __init__(self, devices):
+    def __init__(self, devices: List[Dot]):
         self.devices = devices
 
     def handle_osc_message(self, address, *args):
